efc91613db21aed7453666b5cb158eb69303a086
liyaojinli <307046847@qq.com>
2018-12-16 15:37:46 +0800

    编写bio的客户端程序，并编写测试程序

--- bio/src/main/java/org/liyaojin/study/bio/client/SimpleBioClient.java
+++ bio/src/main/java/org/liyaojin/study/bio/client/SimpleBioClient.java
@@ -1,6 +1,10 @@
 package org.liyaojin.study.bio.client;
 
+import sun.nio.ch.IOUtil;
+
+import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.net.SocketAddress;
 
 /**
@@ -11,13 +15,45 @@ import java.net.SocketAddress;
  * @date 2018/11/24 17:30
  */
 public class SimpleBioClient {
-    private final SocketAddress remote;
+    private Socket socket;
 
     public SimpleBioClient(String host, int port) {
-        this.remote = new InetSocketAddress(host, port);
+        BufferedReader reader = null;
+        try {
+            this.socket = new Socket(host,port);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            System.out.println("connect server success, and the server say:" + reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void start(){
-        
+    public void close(){
+        if(null != socket){
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public String writeAndShowResponse(String toServer){
+        String reply = "";
+        if(null != socket){
+            PrintWriter pw = null;
+            BufferedReader reader = null;
+            try {
+                pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()),true);
+                pw.println(toServer);
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                reply = reader.readLine();
+                System.out.println("the server reply:" + reply);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return reply;
     }
 }
---
+++ bio/src/test/java/org/liyaojin/study/bio/server/SimpleBioEchoClientTests.java
@@ -0,0 +1,24 @@
+package org.liyaojin.study.bio.server;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.liyaojin.study.bio.client.SimpleBioClient;
+
+/**
+ * @author LIYAOJIN
+ * @Title: SimpleBioEchoClientTests
+ * @ProjectName io
+ * @Description: TODO
+ * @date 2018/12/16 15:25
+ */
+public class SimpleBioEchoClientTests {
+    @Test
+    public void testWriteAndShowResponse(){
+        SimpleBioClient client = new SimpleBioClient("127.0.0.1",9999);
+        for(int i = 0 ; i < 10 ; i++){
+            String toServer = "this is from client...@" + (i+1);
+            Assert.assertEquals(client.writeAndShowResponse(toServer),toServer);
+        }
+        client.close();
+    }
+}
